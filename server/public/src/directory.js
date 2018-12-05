53f80e61d82247125be9f767f28670685593ff4d
Mihir Kulkarni <mihirterna@users.noreply.github.com>
2018-12-05 10:31:18 +0530

    ok

--- server/public/src/directory.js
+++ server/public/src/directory.js
@@ -1,6 +1,8 @@
 const express    = require('express');
 const router = express.Router();
 const fs = require('fs');
+
+
 router.post('/',function (req,res,next){
 
     //Directory
--- server/t.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let stat = fs.statSync("D:\LocalCloud\\myCloudSite\\src\\body\\card.js");
-if (stat.isFile()) {
-    console.log(
-        "name", stat.name,"type", "f","size", stat.size,"la", stat.mtime,"lm", stat.ctime, "birth",stat.birthtime);
-}
